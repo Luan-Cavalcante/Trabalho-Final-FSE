9684008a878ae757c124134c105879056feaeb3c
Thalisson-Alves <thalisson2030@gmail.com>
2023-02-13 17:06:53 -0300

    light sleep

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include "nvs_flash.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
+#include "esp_sleep.h"
 #include "esp_log.h"
 #include "freertos/semphr.h"
 
@@ -13,9 +14,12 @@
 #include "state.h"
 #include "buzzer.h"
 
+#include "esp32/rom/uart.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/adc.h"
+#include "driver/rtc_io.h"
 
 #define LDR ADC1_CHANNEL_7
 
@@ -26,6 +30,8 @@
 #define FAROL 2
 #define SENSOR_P 36
 #define BUZZER 23
+
+#define BOTAO_BOOT 0
 
 #define DHT11_PIN 5
 
@@ -44,25 +50,30 @@ void conectadoWifi(void *params)
   }
 }
 
-void trataComunicacaoComServidor(void *params)
+void sendStateToDashboard()
 {
   char mensagem[256];
   struct State *state = getState();
 
+  sprintf(mensagem, "{\"temperatura\": %d, \"umidade\": %d}", state->temperature, state->humidity);
+  mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
+
+  sprintf(mensagem, "{\"luz\":%d,\"ledFarol\":%d,\"farolManual\":%d,\"lowpower\":%d}", state->light, state->headlightOn, state->headlightManual, state->lowPowerMode);
+  mqtt_envia_mensagem("v1/devices/me/attributes", mensagem);
+
+  ESP_LOGI("State", "(buzzerOn=%d,headlightOn=%d,headlightManual=%d,lowPowerMode=%d)", state->buzzerOn, state->headlightOn, state->headlightManual, state->lowPowerMode);
+  ESP_LOGI("State", "(temperature=%d,humidity=%d,light=%d,obstacle=%d)", state->temperature, state->humidity, state->light, state->obstacle);
+}
+
+void trataComunicacaoComServidor(void *params)
+{
   if (xSemaphoreTake(conexaoMQTTSemaphore, portMAX_DELAY))
   {
     while (true)
     {
       vTaskDelay(1000 / portTICK_PERIOD_MS);
 
-      sprintf(mensagem, "{\"temperatura\": %d, \"umidade\": %d}", state->temperature, state->humidity);
-      mqtt_envia_mensagem("v1/devices/me/telemetry", mensagem);
-
-      sprintf(mensagem, "{\"luz\":%d,\"ledFarol\":%d,\"farolManual\":%d}", state->light, state->headlightOn, state->headlightManual);
-      mqtt_envia_mensagem("v1/devices/me/attributes", mensagem);
-
-      ESP_LOGI("State", "(buzzerOn=%d,headlightOn=%d,headlightManual=%d,lowPowerMode=%d)", state->buzzerOn, state->headlightOn, state->headlightManual, state->lowPowerMode);
-      ESP_LOGI("State", "(temperature=%d,humidity=%d,light=%d,obstacle=%d)", state->temperature, state->humidity, state->light, state->obstacle);
+      sendStateToDashboard();
     }
   }
 }
@@ -126,9 +137,48 @@ void TrataGPIO()
 
     // buzzer
     if (state->buzzerOn)
-      make_sound(999);
+      make_sound(650);
     else
       stop_sound();
+  }
+}
+
+void light_sleep_mode()
+{
+  esp_rom_gpio_pad_select_gpio(BOTAO_BOOT);
+  gpio_set_direction(BOTAO_BOOT, GPIO_MODE_INPUT);
+
+  gpio_wakeup_enable(BOTAO_BOOT, GPIO_INTR_LOW_LEVEL);
+  esp_sleep_enable_gpio_wakeup();
+
+  struct State *state = getState();
+  while(true)
+  {
+    vTaskDelay(50);
+
+    if (rtc_gpio_get_level(BOTAO_BOOT) == 0)
+    {
+        printf("Aguardando soltar o botão ... \n");
+        do
+        {
+            vTaskDelay(pdMS_TO_TICKS(10));
+        } while (rtc_gpio_get_level(BOTAO_BOOT) == 0);
+
+        state->lowPowerMode = !state->lowPowerMode;
+    }
+
+    if (!state->lowPowerMode)
+      continue;
+
+    printf("Entrando em modo Light Sleep\n");
+
+    sendStateToDashboard();
+
+    // Configura o modo sleep somente após completar a escrita na UART para finalizar o printf
+    uart_tx_wait_idle(CONFIG_ESP_CONSOLE_UART_NUM);
+
+    // Entra em modo Light Sleep
+    esp_light_sleep_start();
   }
 }
 
@@ -161,4 +211,6 @@ void app_main(void)
   xTaskCreate(&TrataGPIO, "Comunicação com as GPIO", 4096, NULL, 1, NULL);
   xTaskCreate(&conectadoWifi, "Conexão ao MQTT", 4096, NULL, 1, NULL);
   xTaskCreate(&trataComunicacaoComServidor, "Comunicação com Broker", 4096, NULL, 1, NULL);
+
+  light_sleep_mode();
 }
